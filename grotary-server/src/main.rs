976f780a1afb8f4005ebcc09043046c1e00c07da
elftausend <skatsacc.gut24@gmail.com>
2022-05-15 22:40:58 +0200

    nonblocking

--- grotary-server/src/main.rs
+++ grotary-server/src/main.rs
@@ -2,9 +2,17 @@ use std::{net::{TcpListener, TcpStream}, io::Read};
 
 fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:12000")?;
+    listener.set_nonblocking(true)?;
 
     for stream in listener.incoming() {
-        handle_client(stream?);
+        match stream {
+            Ok(stream) => handle_client(stream),
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => panic!("encountered IO error: {}", e),
+        }
+        
     }
 
     Ok(())
@@ -16,7 +24,9 @@ fn handle_client(mut stream: TcpStream) {
 
     loop {
         match stream.read_to_end(&mut data) {
-            Ok(_) => todo!(),
+            Ok(n) => {
+                println!("read {n} bytes");
+            },
             Err(_) => todo!(),
         }
     }
